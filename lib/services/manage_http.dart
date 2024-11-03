58fcaa29ab37eed15f7336073773a7ca8a16d61c
Saubhagya Singh <saubhagyasingh65@gmail.com>
2024-11-03 16:18:53 +0530

    Initial commit

---
+++ lib/services/manage_http.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void manageHttp({
+  required http.Response response,
+  required BuildContext context,
+  required VoidCallback onSuccess,
+}) {
+  switch (response.statusCode) {
+    case 200:
+      onSuccess();
+      break;
+    case 201:
+      onSuccess();
+      break;
+    case 400:
+      showSnackBar(context, jsonDecode(response.body)['msg']);
+      break;
+    case 500:
+      showSnackBar(context, jsonDecode(response.body)['error']);
+      break;
+  }
+}
+
+void showSnackBar(BuildContext context, String title) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(title)));
+}
